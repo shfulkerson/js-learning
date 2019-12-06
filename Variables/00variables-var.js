455f4dd025e08b07b449b1042ab85ddac9a50b1b
shfulkerson <shawn@emberlearninglabs.com>
2019-12-06 10:09:38 -0600

    first commit

---
+++ Variables/00variables-var.js
@@ -0,0 +1,26 @@
+/*****Place your heading here*****/
+
+//Variables are a programming construct which allows the storing of data
+//The data can be in multiple formats
+  //In javascript, the datatype (ie number, letters, etc...) isn't determined until
+  //the variable is assigned
+
+//here we have declared and initilized two different variables of two different types
+//we are using the keyword var (prior to ES6, this was the only way to declare a variable in JS)
+    //var stands for variable
+    //when you use the keyword var, the scope of the variable is global (we will learn more about this later)
+var myName = 'Fulkerson';   //variables myName and favWord have been set up as "String" variables
+var favWord = 'sequester';
+var favNum = 5280;          //favNum is a variable with the type: number
+
+//the following three lines of code print our variables to the console
+
+console.log(myName);
+console.log(favWord);
+console.log(favNum);
+
+/**
+ * place your code here
+ * Expectations: write a code snippet which creates four variables and prints them
+ * to the console
+ */
