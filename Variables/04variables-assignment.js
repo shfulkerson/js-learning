c683c24daaa8620188fd9dc9d7108d8460a49de9
shfulkerson <shawn@emberlearninglabs.com>
2019-12-12 15:46:31 -0600

    added variable-assignments

---
+++ Variables/04variables-assignment.js
@@ -0,0 +1,3 @@
+//Create a javascript and an html file in 04variables-assignment.js and 04variables-assignment.HTML
+//Your completed web page should create 4 different variables using javascript and display them,
+//formatted, using the html file.
