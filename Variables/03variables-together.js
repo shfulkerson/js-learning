8748ed721a8a16d9e56efae3db14b66e0dfac434
shfulkerson <shawn@emberlearninglabs.com>
2019-12-12 13:01:49 -0600

    add recommended packages

--- Variables/03variables-together.js
+++ Variables/03variables-together.js
@@ -2,19 +2,18 @@
  * Place your heading here
  */
 
-//
 
-console.log(varVariable);   //OUTPUT from the following 3 lines:
-var varVariable = 32;       //undefined
-console.log(varVariable);   //32
-//even though the variable varVariable isn't declared until the line after the
-//first console log, this doesn't create an error because var declares the
-//variable globally
+var varVariable = 32;       //number variable
+let letVariable = 'This is a variable declared using let'; //string variable
+const constVariable = true //constant variable
+
+
 
 //Uncomment the following line when you are ready to test let
 //console.log(letVariable);
 let letVariable = 64;
 console.log(letVariable);
+
 //Once you uncomment line 19 and run the program, you will recieve an error
 //
 
