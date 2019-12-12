49a9ba63e4411b0bb48e1cec98150d8686bc6f90
shfulkerson <shawn@emberlearninglabs.com>
2019-12-12 14:49:09 -0600

    update variables-together

--- Variables/03variables-together.js
+++ Variables/03variables-together.js
@@ -2,20 +2,22 @@
  * Place your heading here
  */
 
+//In this program we will create some variables using var, let, and const
+//We will then use those variables to update some HTML elements
 
-var varVariable = 32;       //number variable
-let letVariable = 'This is a variable declared using let'; //string variable
-const constVariable = true //constant variable
+//var variable assigned to a string:
+var message = "We like variables. This text was created in a javascript file";
 
+//let variable assigned to a number:
+let theAnswer = 42;
 
+//2 const variables to which are assigned html elements
+//those elements are accessed by their html ids using the document object and the getElementById method
+const messageElement = document.getElementById('message');
+const answerElement = document.getElementById('answer');
 
-//Uncomment the following line when you are ready to test let
-//console.log(letVariable);
-let letVariable = 64;
-console.log(letVariable);
-
-//Once you uncomment line 19 and run the program, you will recieve an error
-//
+messageElement.innerHTML = message;
+answerElement.innerHTML = theAnswer;
 
 /**
  * place your code here
